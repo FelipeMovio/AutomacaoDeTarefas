72c9561131d086bc7c6080267e717b60f2794c28
FelipeMovio <felipelsmovio@gmail.com>
2025-09-15 20:38:07 -0300

    fazendo loguin

--- codigo.py
+++ codigo.py
@@ -1,7 +1,3 @@
-# passo 1 - Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
-                                           #  exemplo qualquer de site
-# passo 2 - Fazer login 
-# passo 3 - importar base de dados 
 # passo 4 - cadastrar um produto 
 # passo 5 - Repetir para todos os produtos 
 
@@ -10,19 +6,28 @@ import pyautogui
 # pyautogui.write - escrever algo
 # pyautogui.click - clicar em algum lugar
 
+import time
+
 pyautogui.PAUSE = 0.5  # configuração de tempo entre execuçoes
 
 
-# passo 1
+# passo 1 - Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
-pyautogui.sleep(2)
 
-# passo 2
+time.sleep(2)
+
+# passo 2 - Fazer login 
 pyautogui.press("tab")
 pyautogui.write("email@email.com")
 pyautogui.press("tab")
-pyautogui.write("1234")
+pyautogui.write("senhasuperfacil")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+time.sleep(2)
+
+# passo 3 - importar base de dados 
